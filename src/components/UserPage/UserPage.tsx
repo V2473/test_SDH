f76b021a29c917a39d72ca6f34cdd9b175c50b25
Vovasef <vovasef@gmail.com>
2021-09-28 15:29:56 +0300

    v1.2

--- src/components/UserPage/UserPage.tsx
+++ src/components/UserPage/UserPage.tsx
@@ -28,6 +28,8 @@ const UserPage = (props: Props): JSX.Element => {
   const createUser = () => dispatch(actions.createUser(user))
   const deleteUser = () => dispatch(actions.deleteUser(user.id))
   const history = useHistory();
+  const location = useLocation();
+
 
 
   const inputHandler = (e: InputTypes): void => {
@@ -62,21 +64,26 @@ const UserPage = (props: Props): JSX.Element => {
   return (
     <div className={classnames('card', "userPage")}>
       <div className="userCard-id">
-        <span className="userCard-id-text">ID: 
-          {user.id !== undefined ? user.id : (
-            <button onClick={() =>{
-              createUser()
-              history.push('/contact')
-            }}
-            className={classnames('btn', 'btn-success')}
-            disabled={isEmptyField}
-            >CREATE USER</button>
-          )} 
-        </span>
-
-        {user.id ? (
+      {user.id !== undefined ? user.id : 
+        location.pathname.slice(9) === 'create' ? 
+          (
+            <span className="userCard-id-text"> ID:
+              <button onClick={() =>{
+                createUser()
+                history.push('/contact')
+              }}
+              className={classnames('btn', 'btn-success')}
+              disabled={isEmptyField}
+              >CREATE USER</button>
+            </span>
+          ) : (
+            <></>
+          )
+        } 
+        {user.id !== undefined || location.pathname.slice(9) === 'create' ? (
           <>
-            {editMode ? (
+
+            {location.pathname.slice(9) === 'create' ? '' : editMode ? (
               <>
                 <button onClick={() => {
                   setEditMode(false)
@@ -107,155 +114,154 @@ const UserPage = (props: Props): JSX.Element => {
                 className={classnames('btn', 'btn-outline-warning')}
               >EDIT</button>
             )}
-          </>
-        ) : (
-          <>
-            
-          </>
-        )}
-      </div>
-      <div className="userPage-info">
-        {editMode ? (
-          <form>
-          <label htmlFor="first_name">
-            <span className="userCard-info-first_name">Name: 
-          </span>
-            <input
-              type='text'
-              name='first_name'
-              value={user.first_name}
-              onChange={inputHandler}
-              className={classnames('form-control')}
-              placeholder='required field'
-              maxLength={256}
-            ></input>
-          </label>
-          <br />
-          <label htmlFor="last_name">
-            <span className="userPage-info-last_name">Surname: </span>
-            <input
-              type='text'
-              name='last_name'
-              value={user.last_name}
-              onChange={inputHandler}
-              className={classnames('form-control')}
-              placeholder='required field'
-              maxLength={256}
-            ></input>
-          </label>
-          <br />
-          <label htmlFor="birth_date">
-            <span className="userPage-info-desc">birth_date: </span>
-            <input
-              type='date'
-              name='birth_date'
-              value={user.birth_date}
-              onChange={(e) => inputHandler(e)}
-              className={classnames('form-control')}
-              placeholder='required field'
-            ></input>   
-          </label>
-          <br />
-          <label htmlFor="gender">
-            <span className="userPage-info-desc">gender: </span>
-            <select
-              name="gender"
-              onChange={(e) => inputHandler(e)}
-              value={user.gender}
-            >
-              <option value="" >Select gender</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
-          </label>
-          <br />
-          <label htmlFor="job">
-            <span className="userPage-info-job">job: </span>
-            <input
-              type='text'
-              name='job'
-              value={user.job}
-              onChange={(e) => inputHandler(e)}
-              className={classnames('form-control')}
-              placeholder='required field'
-              maxLength={256}
-            ></input>   
-          </label>
-          <br />
-          <label htmlFor="biography">
-            <span className="userPage-info-biography">biography: </span>
-            <textarea
-              name='biography'
-              value={user.biography}
-              onChange={(e) => inputHandler(e)}
-              className={classnames('form-control')}
-              placeholder='required field'
-              maxLength={1024}
-            ></textarea>   
-          </label>
-          <br />
-          <label htmlFor="is_active" className={classnames('form-check-label')}
-          >
-          <input
-              type='checkbox'
-              name='is_active'
-              id='is_active'
-              checked={user.is_active}
-              onChange={(e) => inputHandler(e)}
-              className={classnames('form-check-input', )}
-              placeholder='required field'
-            ></input>   
-            <span className="userPage-info-is_active"> is_active </span>
-          </label>
-        </form>
-        ) : (
-          <form>
-          <label htmlFor="first_name">first_name: <br />
-            <span className="userPage-info-first_name">
-              {user.first_name} 
-          </span>
-          </label>
-          <br />
-          <label htmlFor="last_name">last_name: <br />
-            <span className="userPage-info-last_name"> 
-            { user.last_name}  
-            </span>
-          </label>
-          <br />
-          <label htmlFor="birth_date">birth_date: <br />
-            <span className="userPage-info-birth_date">
-              {user.birth_date} 
-            </span> 
-          </label>
-          <br />
-          <label htmlFor="gender">gender: <br />
-            <span className="userPage-info-gender">
-              {user.gender} 
-            </span> 
-          </label>
-          <br />
-          <label htmlFor="job">job: <br />
-            <span className="userPage-info-job">
-              {user.job} 
-            </span> 
-          </label>
-          <br />
-          <label htmlFor="biography">biography:<br />
-            <span className="userPage-info-biography"> 
-              {user.biography} 
-            </span> 
-          </label>
-          <br />
-          <label htmlFor="is_active">
-              {user.is_active ? 'active' : 'not active'}
-          </label>
-        </form>
-        )}
-        
-      </div>
 
+            <div className="userPage-info">
+                {editMode ? (
+                  <form>
+                  <label htmlFor="first_name">
+                    <span className="userCard-info-first_name">Name: 
+                  </span>
+                    <input
+                      type='text'
+                      name='first_name'
+                      value={user.first_name}
+                      onChange={inputHandler}
+                      className={classnames('form-control')}
+                      placeholder='required field'
+                      maxLength={256}
+                    ></input>
+                  </label>
+                  <br />
+                  <label htmlFor="last_name">
+                    <span className="userPage-info-last_name">Surname: </span>
+                    <input
+                      type='text'
+                      name='last_name'
+                      value={user.last_name}
+                      onChange={inputHandler}
+                      className={classnames('form-control')}
+                      placeholder='required field'
+                      maxLength={256}
+                    ></input>
+                  </label>
+                  <br />
+                  <label htmlFor="birth_date">
+                    <span className="userPage-info-desc">birth_date: </span>
+                    <input
+                      type='date'
+                      name='birth_date'
+                      value={user.birth_date}
+                      onChange={(e) => inputHandler(e)}
+                      className={classnames('form-control')}
+                      placeholder='required field'
+                    ></input>   
+                  </label>
+                  <br />
+                  <label htmlFor="gender">
+                    <span className="userPage-info-desc">gender: </span>
+                    <select
+                      name="gender"
+                      onChange={(e) => inputHandler(e)}
+                      value={user.gender}
+                    >
+                      <option value="" >Select gender</option>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                    </select>
+                  </label>
+                  <br />
+                  <label htmlFor="job">
+                    <span className="userPage-info-job">job: </span>
+                    <input
+                      type='text'
+                      name='job'
+                      value={user.job}
+                      onChange={(e) => inputHandler(e)}
+                      className={classnames('form-control')}
+                      placeholder='required field'
+                      maxLength={256}
+                    ></input>   
+                  </label>
+                  <br />
+                  <label htmlFor="biography">
+                    <span className="userPage-info-biography">biography: </span>
+                    <textarea
+                      name='biography'
+                      value={user.biography}
+                      onChange={(e) => inputHandler(e)}
+                      className={classnames('form-control')}
+                      placeholder='required field'
+                      maxLength={1024}
+                    ></textarea>   
+                  </label>
+                  <br />
+                  <label htmlFor="is_active" className={classnames('form-check-label')}
+                  >
+                  <input
+                      type='checkbox'
+                      name='is_active'
+                      id='is_active'
+                      checked={user.is_active}
+                      onChange={(e) => inputHandler(e)}
+                      className={classnames('form-check-input', )}
+                      placeholder='required field'
+                    ></input>   
+                    <span className="userPage-info-is_active"> is_active </span>
+                  </label>
+                </form>
+                ) : (
+                  <form>
+                  <label htmlFor="first_name">first_name: <br />
+                    <span className="userPage-info-first_name">
+                      {user.first_name} 
+                  </span>
+                  </label>
+                  <br />
+                  <label htmlFor="last_name">last_name: <br />
+                    <span className="userPage-info-last_name"> 
+                    { user.last_name}  
+                    </span>
+                  </label>
+                  <br />
+                  <label htmlFor="birth_date">birth_date: <br />
+                    <span className="userPage-info-birth_date">
+                      {user.birth_date} 
+                    </span> 
+                  </label>
+                  <br />
+                  <label htmlFor="gender">gender: <br />
+                    <span className="userPage-info-gender">
+                      {user.gender} 
+                    </span> 
+                  </label>
+                  <br />
+                  <label htmlFor="job">job: <br />
+                    <span className="userPage-info-job">
+                      {user.job} 
+                    </span> 
+                  </label>
+                  <br />
+                  <label htmlFor="biography">biography:<br />
+                    <span className="userPage-info-biography"> 
+                      {user.biography} 
+                    </span> 
+                  </label>
+                  <br />
+                  <label htmlFor="is_active">
+                      {user.is_active ? 'active' : 'not active'}
+                  </label>
+                </form>
+                )}
+                
+              </div>
+          </>
+          ) : (
+              
+          <></>
+        )}
+      </div>
     </div>
-  )
+  )    
 }
 
 export default UserPage;
