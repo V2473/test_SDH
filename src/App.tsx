900c9fae26882635543e0878599ccc1bfaae2582
Vovasef <vovasef@gmail.com>
2021-09-29 16:29:44 +0300

    v1.2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import Contacts from './components/Constacts/Contacts';
+import Contacts from './components/Contacts/Contacts';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 
